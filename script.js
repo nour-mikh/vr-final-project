2608b4b136294d03e8114f6ecb66c74436bf5de0
Nour <nourmikhael@hotmail.com>
2023-11-09 21:34:04 +0200

    Adding objects drop

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 AFRAME.registerComponent('moon', {
     schema: {
-      color: {default: 'red'}
     },
 
     init: function () {
-      var data = this.data;
       var el = this.el; 
 
       let moonCard = document.getElementById("moonCard");
 
-
       el.addEventListener('mouseenter', function () {
-        moonCard.setAttribute('visibility', 'visible');
+        moonCard.style.visibility = 'visible';
+        //moonCard.object3D.visible = false;
+        console.log("clicked");
       });
 
       el.addEventListener('mouseleave', function () {
-        moonCard.setAttribute('visibility', 'hidden');
+        moonCard.style.visibility = 'hidden';
+        //moonCard.object3D.visible = true;
+        console.log("left");
+
       });
     }
   });
